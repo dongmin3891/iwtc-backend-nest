0a0a05d7111ad7b681e21eb265d31758be9d4c4e
dongminkim <65215746+dongmin3891@users.noreply.github.com>
2026-09-17 08:54:01 +0900

    feat: register automation world cup endpoints

--- src/manage-world-cups/manage-world-cups.module.ts
+++ src/manage-world-cups/manage-world-cups.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module.js';
 import { MediaFilesModule } from '../media-files/media-files.module.js';
+import { AutomationGuard } from './automation.guard.js';
+import { AutomationWorldCupsController } from './automation-world-cups.controller.js';
 import { ManageWorldCupContentsController } from './manage-world-cup-contents.controller.js';
 import { ManageWorldCupContentsService } from './manage-world-cup-contents.service.js';
 import { ManageWorldCupsController } from './manage-world-cups.controller.js';
@@ -8,7 +10,15 @@ import { ManageWorldCupsService } from './manage-world-cups.service.js';
 
 @Module({
   imports: [AuthModule, MediaFilesModule],
-  controllers: [ManageWorldCupsController, ManageWorldCupContentsController],
-  providers: [ManageWorldCupsService, ManageWorldCupContentsService],
+  controllers: [
+    ManageWorldCupsController,
+    ManageWorldCupContentsController,
+    AutomationWorldCupsController,
+  ],
+  providers: [
+    ManageWorldCupsService,
+    ManageWorldCupContentsService,
+    AutomationGuard,
+  ],
 })
 export class ManageWorldCupsModule {}
